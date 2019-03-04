05e110d129130471a1b0802a9bf73fea81c3dd4b
TIANyunyi <47521333+TIANyunyi@users.noreply.github.com>
2019-03-05 01:38:36 +0800

    Voter register and login function
    Voter can register an account now, which  can login in the main system

--- VotingSystem/VotingSystem/Login.cs
+++ VotingSystem/VotingSystem/Login.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace VotingSystem
 {
     public partial class Login : Form
@@ -15,6 +15,66 @@ namespace VotingSystem
         public Login()
         {
             InitializeComponent();
+        }
+        string strcon, strsql;
+        SqlConnection mycon;
+        SqlCommand command;
+        private bool DBConnect()
+        {
+            try
+            {
+                strcon = "Data Source=DESKTOP-6UGITVT;Initial Catalog=Voting;Integrated Security=True";
+
+                mycon = new SqlConnection(strcon);
+                mycon.Open();
+                MessageBox.Show("DB Connect is good");
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("DB Connect is not connect");
+
+                return false;
+            }
+        }
+        private bool check()
+        {
+            if (textBox1.Text.Length == 0)
+            {
+                textBox1.Select();
+                return false;
+            }
+            if (textBox2.Text.Length == 0)
+            {
+                textBox1.Select();
+                return false;
+            }
+            return true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (check() && DBConnect())
+            {
+                strsql = string.Format("select count (*) from Voter where Name = '{0}' and Password = '{1}'", textBox1.Text, textBox2.Text);
+                command = new SqlCommand(strsql, mycon);
+                try
+                {
+                    int result = Convert.ToInt32(command.ExecuteScalar());
+                    if (result > 0)
+                    {
+                        MessageBox.Show("successful login");
+                       
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login failed");
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Sql error");
+                }
+            }
         }
     }
 }
--- VotingSystem/VotingSystem/Register.cs
+++ VotingSystem/VotingSystem/Register.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace VotingSystem
 {
     public partial class Register : Form
@@ -15,6 +15,89 @@ namespace VotingSystem
         public Register()
         {
             InitializeComponent();
+        }
+        string strcon, strsql;
+        SqlConnection mycon;
+        SqlCommand command;
+        DataSet ds;
+        SqlDataAdapter da;
+        private bool DBConnect()
+        {
+            try
+            {
+                strcon = "Data Source=DESKTOP-6UGITVT;Initial Catalog=Voting;Integrated Security=True";
+
+                mycon = new SqlConnection(strcon);
+                mycon.Open();
+                MessageBox.Show("DB Connect is good");
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("DB Connect is not connect");
+
+                return false;
+            }
+        }
+        private bool check()
+        {
+            if (textBox1.Text.Length == 0)
+            {
+                textBox1.Select();
+                return false;
+            }
+            if (textBox2.Text.Length == 0)
+            {
+                textBox1.Select();
+                return false;
+            }
+            if (textBox3.Text.Length == 0)
+            {
+                textBox1.Select();
+                return false;
+            }
+            if (textBox4.Text.Length == 0)
+            {
+                textBox1.Select();
+                return false;
+            }
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            this.Hide();
+            login.ShowDialog(this);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DBConnect();
+            strsql = string.Format("insert into Voter(Name,Password,Email,Age) values('{0}','{1}','{2}','{3}')",textBox1.Text,textBox2.Text,textBox3.Text,textBox4.Text);
+            MessageBox.Show(strsql);
+            command = new SqlCommand(strsql, mycon);
+            try
+            {
+                command.ExecuteScalar();
+                MessageBox.Show("Register successful");
+                Login login = new Login();
+                this.Hide();
+                login.ShowDialog(this);
+            }
+            catch
+            {
+                MessageBox.Show("Register failed");
+            }
+            finally
+            {
+                mycon.Close();
+            }
         }
     }
 }
