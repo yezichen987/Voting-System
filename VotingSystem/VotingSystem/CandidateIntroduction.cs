2e66c3c04c6b52659a68c826710f39298a28202b
TIANyunyi <47521333+TIANyunyi@users.noreply.github.com>
2019-03-13 01:42:22 +0800

    CandidateIntro Form
    Function design, pictureBox

--- VotingSystem/VotingSystem/CandidateIntroduction.cs
+++ VotingSystem/VotingSystem/CandidateIntroduction.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.IO;
+
 namespace VotingSystem
 {
     public partial class CandidateIntroduction : Form
@@ -34,11 +36,12 @@ namespace VotingSystem
             CandidateInformation candidateInformation = new CandidateInformation();
             this.Close();
             candidateInformation.ShowDialog();
+
         }
 
         private void label2_Click(object sender, EventArgs e)
         {
-
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -48,24 +51,56 @@ namespace VotingSystem
 
         private void CandidateIntroduction_Load(object sender, EventArgs e)
         {
-            label1.Text = DateTime.Now.ToString();
-            byte[] imagebytes = null;
-            //打开数据库
-            SqlConnection con = new SqlConnection("Data Source=DESKTOP-6UGITVT;Initial Catalog=Voting;Integrated Security=True");
-            con.Open();
-            SqlCommand com = new SqlCommand("select Image from Candidate where Id=1", con);
-            SqlDataReader dr = com.ExecuteReader();
-            while (dr.Read())
-            {
-                imagebytes = (byte[])dr.GetValue(1);
-            }
-            dr.Close();
-            com.Clone();
-            con.Close();
-            CandidateIntroduction candidate = new CandidateIntroduction(imagebytes);
-            Bitmap bmpt = new Bitmap(candidate.ToString());
-            pictureBox1.Image = bmpt;
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-6UGITVT;Initial Catalog=Voting;Integrated Security=True");
+
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("select Image from CandidateImage where Id='1'", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            MemoryStream buf = new MemoryStream((byte[])reader[0]);
+            Image image = Image.FromStream(buf, true);
+            pictureBox1.Image = image;
+
+        }
+        // public void PicboxShow(PictureBox pictureBox2)
+        //{
+        //    byte[] imagebytes = null;
+
+        //    //打开数据库
+
+        //    SqlConnection con = new SqlConnection("server=.;database=PointSchool;User =sa; pwd =123");
+
+        //    con.Open();
+
+        //    SqlCommand com = new SqlCommand("select top 1* from CandidateImage_Image", con);
+
+        //    SqlDataReader dr = com.ExecuteReader();
+
+        //    while (dr.Read())
+        //    {
+
+        //        imagebytes = (byte[])dr.GetValue(1);
+
+        //    }
+
+        //    dr.Close();
+
+        //    com.Clone();
+
+        //    con.Close();
+
+        //    CandidateIntroduction ms = new CandidateIntroduction(imagebytes);
+
+        //    Bitmap bmpt = new Bitmap(ms.ToString());
+
+        //    pictureBox2.Image = bmpt;
+        //    pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+        //}
+
+ 
+       
 
         }
     }
-}
+
